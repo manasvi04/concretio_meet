b64cc6d03c46a6f4d658a31f2ac05f425ac3e6f8
Akshat Porwal <akshatporwal022003@gmail.com>
2025-09-11 18:59:37 +0530

    chore: add ImportMetaEnv type definition for room password environment variable

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CREATE_ROOM_PASSWORD: string
+  // Add other environment variables here as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}

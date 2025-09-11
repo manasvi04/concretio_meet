6c0eba0e89c11ff453b7bfa64d3f22bc5ecbd949
Akshat Porwal <akshatporwal022003@gmail.com>
2025-09-11 10:59:15 +0530

    fix: update button label from "Join Room" to "Join Call"

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -237,7 +237,7 @@ const Index = () => {
                   ) : (
                     <>
                       <Phone className="w-4 h-4 mr-2"/>
-                      Join Room
+                      Join Call
                     </>
                   )}
                 </Button>
